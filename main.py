d4b2c25e96540462a3b84ea89c25d7b29b79ce16
tongplw <bangkok.tong@gmail.com>
2020-06-27 03:13:29 +0700

    update mindwave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 import mindwave
 import pandas as pd
+from datetime import datetime
 
 
 headset = mindwave.Headset('COM6')
@@ -11,19 +12,22 @@ wave = headset.waves
 keys = ['raw', 'attention', 'meditation'] + list(wave.keys())
 print(''.join(f'{k:<14}' for k in keys))
 
+starttime = time.time()
 values = []
 
 while True:
 
+    time.sleep(1/256 - ((time.time() - starttime) % (1/256)))
+
 	# print values
     wave = headset.waves
-    values += [[headset.raw_value, headset.attention, headset.meditation] + list(wave.values())]
-    print(''.join(f'{v:<14}' for v in values[-1]), end='\r')
-
+    values += [[datetime.now()] + [headset.raw_value, headset.attention, headset.meditation] + list(wave.values())]
+    # print(''.join(f'{v:<14}' for v in values[-1]), end='\r')
+    
     # save data every 10 lines
-    if len(values) % 10 == 0:
+    if len(values) % 1024 == 0:
         df = pd.DataFrame(values)
-        df.to_csv('data.csv', mode='a', index=False, header=False)
+        df.to_csv('raw.csv', mode='a', index=False, header=False)
         values = []
     
-    time.sleep(1)
+    # time.sleep(1/256)
